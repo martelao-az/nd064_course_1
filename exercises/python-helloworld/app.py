f5ef5d7779bad3670db5affcf8bd2d3df853b1b0
martelao-az <82973574+martelao-az@users.noreply.github.com>
2021-06-22 12:45:20 -0700

    Update app.py

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,30 @@
 from flask import Flask
+from flask import json
+ 
+
 app = Flask(__name__)
+
+@app.route('/status')
+def status(): 
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"})
+        status= 200,
+        mimetype='application/json'
+    )
+
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"status":"success","code":0,data":{UserCount":140, "UserCountActive":23}}),
+    )
+
+return Response
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-if __name__ == "__main__":
+if _name_== "_main_":
     app.run(host='0.0.0.0')
